5280e37ca6afb07c51e15bd7e1a40b43b71f423b
vivanraj81 <141013692+vivanraj81@users.noreply.github.com>
2024-01-08 15:18:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,7 @@
 // ReactDOM.render(React.createElement(Container),container);
 
 class ReactContainer extends React.Component{
-    // constructor - to initialize the state 
-  
-    // render method to render the react dom 
+ 
       render(){
           return React.createElement(`div`,null,`Hey Kalvians`,
           React.createElement(`div`,null,`Let's rock and roll with classes`)
